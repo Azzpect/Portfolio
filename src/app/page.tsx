3e1e71cdd1cb54513cae2d5019b5d3ee32fdf872
Azzpect <atanughosh.brp@gmail.com>
2024-12-04 15:30:59 +0530

    added inotes app and weather app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,9 @@ const kanit = Kanit({
 export default function Home() {
 
   const projectData: {title: string, desc: string, img: string, repoLink: string}[] = [
-    {title: "ChatAPP", desc: "A real-time chat application built using Node.js, Socket.io, and React.js. The application allows users to chat in real-time with other users also send friend requests and accept them.", img: "/chatApp.png", repoLink: "https://github.com/Azzpect/ChatAPP.git"},
-    {title: "INotesApp", desc: "A note-taking application built using Node.js, Express.js, and React.js. The application allows users to create, edit, and delete notes, and also search for notes by title or content.", img: "/chatApp.png", repoLink: "https://github.com/Azzpect/iNotes.github.io"}
+    {title: "ChatAPP", desc: "A real-time chat application built using Node.js, Socket.io, and React.js. The application allows users to chat in real-time with other users also send friend requests and accept them.", img: "/chatApp.png", repoLink: "https://github.com/Azzpect/ChatAPP"},
+    {title: "INotesApp", desc: "A note-taking application built using Node.js, Express.js, and React.js. The application allows users to create, edit, and delete notes, and also search for notes by title or content.", img: "/inotesApp.png", repoLink: "https://github.com/Azzpect/iNotes.github.io"}, 
+    {title: "WeatherApp", desc: "A weather application built using React.js. The application allows users to search for weather information by city name.", img: "/weatherApp.png", repoLink: "https://github.com/Azzpect/WeatherApp"}
   ]
 
   return (
@@ -89,14 +90,14 @@ export default function Home() {
         </div>
       </div>
     </div>
-    <div id="projects" className="w-screen border-solid mb-28 flex flex-col items-center underline decoration-white relative">
+    <div id="projects" className="w-screen border-solid mb-28 flex flex-col flex-wrap items-center underline decoration-white relative">
       <h1 className={`text-white text-3xl font-bold ${poppins.className} p-10`}>Projects</h1>
       <div className="flex justify-around w-full">
         {projectData.map((project, index) => {
           return <ProjectCard key={index} title={project.title} desc={project.desc} img={project.img} repoLink={project.repoLink} />
         })}
       </div>
-      <Link href="#main" className="absolute -bottom-14 right-20 rounded-full">
+      <Link href="#main" className="absolute -bottom-14 right-20 rounded-full z-20">
         <div className="rotater"></div>
         <div className="bg-primaryBgColor rounded-full">
           <Image src="/upArrow.svg" alt="Up arrow" width={50} height={50}/>
@@ -115,7 +116,7 @@ function ProjectCard({title, desc, img, repoLink}: {title: string, desc: string,
         <div className="rotater"></div>
       <div className="absolute w-full h-full bg-primaryBgColor p-3 flex flex-col">
         <h1 className={`text-white text-2xl font-semibold ${poppins.className} py-2`}>{title}</h1>
-        <Link href="/chatApp.png" className="self-center"><Image src={img} alt={title} width={200} height={200} /></Link>
+        <Link href={img} className="self-center"><Image src={img} alt={title} width={200} height={200} /></Link>
         <p className={`text-[#9b9b9b] text-xs ${kanit.className} py-2`}>{desc}</p>
         <Link href={repoLink} className={`text-white text-sm font-semibold ${kanit.className} self-end`}>Visit Repository&gt;&gt;</Link>
       </div>
